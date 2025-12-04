6aaf079219c820f0365e034c307b8a9e026ea066
FireCoder-N <dimnestmanolis@gmail.com>
2025-12-04 21:19:34 +0200

    Day4, task2

---
+++ day4/task2.py
@@ -0,0 +1,36 @@
+import os
+
+data = []
+total = 0
+
+with open(os.path.dirname(os.path.realpath(__file__)) + "/input.txt", "r") as file:
+    for line in file:
+        data.append(["."] + [i for i in line.strip()]+ ["."])
+
+    data.insert(0, ["." for i in range(len(data[0]))])
+    data.append(["." for i in range(len(data[0]))])
+
+    while True:
+        toRemove = []
+
+        for i in range(1, len(data)-1):
+            for j in range(1, len(data[0])-1):
+                if data[i][j] == "@":
+
+                    if [
+                        data[i-1][j-1], data[i-1][j], data[i-1][j+1],
+                        data[i][j-1], data[i][j+1],
+                        data[i+1][j-1], data[i+1][j], data[i+1][j+1]
+                    ].count("@") < 4:
+                        toRemove.append((i,j))
+
+        if len(toRemove) == 0:
+            break
+        
+        total += len(toRemove)
+        
+        for i,j in toRemove:
+            data[i][j] = "X"
+
+print(total)
+
